f8e80e67df62a31ddef6c577c8c2266039b22177
Kirill Greshnov <k.greshnov@innopolis.university>
2024-11-02 14:58:05 +0300

    Final

--- aStarProper.cpp
+++ aStarProper.cpp
@@ -1,7 +1,9 @@
+// Kirill Greshnov B23-DSAI-03
 #include <bits/stdc++.h>
 
 using namespace std;
 
+// coord_t struct holds the coordinates of a cell
 struct coord_t {
     int x;
     int y;
@@ -11,6 +13,18 @@ struct coord_t {
     }
 };
 
+/*
+    node_a struct represents a node/cell object
+
+    Variables:
+    pos - coordinates of a node
+    fVal, gVal, hVal - values of f(n), g(n), h(n) functions
+        for this node
+    danger - 'true' if this node is dangerous to the Actor
+        (Enemy or Perseption Zone of the Enemy)
+    parent - pointer to the node the algorithm moved from
+        to this node
+*/
 struct node_a {
     coord_t pos;
     int fVal;
@@ -24,11 +38,36 @@ struct node_a {
     }
 };
 
+/*
+    'sorter' function is responsible for correctly sorting the vector of nodes (type node_a) based on the values
+    of functions fVal & hVal: we prioritize the smaller total path-length fVal and, in case they are equal for 2 nodes,
+    we choose one with the smallest hVal, meaning that the node is closer to the end point
+*/
 bool sorter(node_a *a, node_a *b) {
     if (a->fVal == b->fVal) return a->hVal < b->hVal;
     return a->fVal < b->fVal;
 }
 
+/*
+    NeoAStar class constains the implementation of A* algorithm for this particular assignment
+
+    Variables:
+    perceptionVariant - perception variant of the actor for this test
+    map[][] - matrix of node objects (Environment of the game)
+    neoPos - current position of the Actor
+    kmPos - position of Keymaker (end point)
+    bdkPos - position of the Backdoor Key, if found
+    sentinel - pointer to the node containing the Sentinel, if found
+    agents - vector of node pointers to known Agents
+    keyFound - 'true' if the Backdoor Key was found
+    gameInProgress - 'true' if the game is not over
+    moves - vector of node objects representing the list of nodes for the
+        algorithm to check next
+    n - length of a shortest path to Keymaker, or -1 if no valid path
+
+    Functions:
+    (Comments are given above function)
+*/
 class NeoAStar {
 private:
     int perceptionVariant;
@@ -43,6 +82,10 @@ private:
     vector <node_a*> moves;
     int n = -1;
 
+    /*
+        'fillMapValues' fills initial values for the node objects in
+        the map
+    */
     void fillMapValues() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -55,6 +98,12 @@ private:
         map[0][0]->fVal = map[0][0]->gVal + map[0][0]->hVal;
     }
 
+    /*
+        Given 2 nodes 'node' & 'temp', 'fixx' function is used in the following
+        'fixValuesAroundNode' function to change f, g, h values for node temp
+        when making a move node->temp. Then 'fixx' adds 'temp' node to 'moves'
+        and sets 'temp's' parent to be 'node'
+    */
     void fixx(node_a *node, node_a *temp) {
         temp->gVal = node->gVal + 1;
         temp->fVal = temp->gVal + temp->hVal;
@@ -62,6 +111,13 @@ private:
         temp->parent = node;
     }
 
+    /*
+        Given node 'node', 'fixValuesAroundNode' function uses 'fixx' to update the
+        4 nodes adjacent to 'node'.
+        It check whether the adjacent node exists (that it's not outside of the map),
+        whether it can improve g value of the adjacent node.
+        Adjacent node is not considered if it is "dangerous".
+    */
     void fixValuesAroundNode(node_a *node) {
 
         int x = node->pos.x, y = node->pos.y;
@@ -80,31 +136,49 @@ private:
         }
     }
 
+    /*
+        'recreatePath' function is used to recreate the final shortest path from (0;0)
+        to Keymaker and counts the number of steps 'n' it took
+    */
     void recreatePath() {
         node_a *currentNode = map[neoPos.y][neoPos.x];
         n = 0;
         while (currentNode != map[0][0]) {
-            // printf("(%d;%d)--", currentNode->pos.x, currentNode->pos.y);
             currentNode = currentNode->parent;
             n++;
         }
-        // printf("(0;0)\n");
     }
 
+    /*
+        'handleAgent' function changes 'danger' variable of nodes in perception range of
+        the Agent and the node containing Agent itself.
+        If the Backdoor Key is found, then all of the above mentioned nodes are no longer dangerous
+        and their values are changes accordingly.
+        If the Key is not found yet, 'handleAgent' sets 'danger' values of the above mentioned nodes
+        to 'true'
+    */
     void handleAgent(node_a *node) {
         if (!keyFound) {
-            for (int i = max(node->pos.y - 1, 0); i < min(node->pos.y + 1, 9); i++)
-                for (int j = max(node->pos.x - 1, 0); j < min(node->pos.x + 1, 9); j++) {
+            for (int i = max(node->pos.y - 1, 0); i < min(node->pos.y + 1, 8); i++)
+                for (int j = max(node->pos.x - 1, 0); j < min(node->pos.x + 1, 8); j++) {
                     map[i][j]->danger = true;
                 }
             return;
         }
-        for (int i = max(node->pos.y - 1, 0); i < min(node->pos.y + 1, 9); i++)
-            for (int j = max(node->pos.x - 1, 0); j < min(node->pos.x + 1, 9); j++) {
+        for (int i = max(node->pos.y - 1, 0); i < min(node->pos.y + 1, 8); i++)
+            for (int j = max(node->pos.x - 1, 0); j < min(node->pos.x + 1, 8); j++) {
                 map[i][j]->danger = false;
             }
     }
 
+    /*
+        Similarly to 'handleAgent', 'handleSentinel' changes the 'danger' values of the node
+        containing the Sentinel itself, as well as nodes in its perception range.
+        If the Key is found, then, given from the assignment statement, the perception range
+        of the Sentinel is increased.
+        Otherwise, the initial 'danger' values for nodes in Sentinel's perception range are
+        set to 'true'.
+    */
     void handleSentinel(node_a *node) {
         if (!keyFound) {
             if (node->pos.x > 0) {
@@ -133,6 +207,12 @@ private:
                 }
     }
 
+    /*
+        'dataAwait' function get information about nodes in Actor's perception
+        range after every move, it calls handlers for Agents and Sentinel &
+        changes 'danger' values of nodes if they are said to be 'P'erception Zone
+        of the Enemy
+    */
     void dataAwait() {
         int n_inputs;
         cin >> n_inputs;
@@ -159,10 +239,21 @@ private:
         }
     }
 
+    /*
+        'isClose' function simply returns 'true' if the two given
+        nodes 'start' & 'end' are adjacent (or equal) to each other
+    */
     bool isClose(node_a *start, node_a *end) {
         return (abs(start->pos.x - end->pos.x) + abs(start->pos.y - end->pos.y) == 1) || (start == end);
     }
 
+    /*
+        'findIntersection' is a helper function for the following
+        'aToB' function & returns a node - intersection of paths
+        from a & from b.
+
+        Detailed explanation if comments to 'aToB' function.
+    */
     node_a *findIntersection(node_a *a, node_a *b) {
         vector<node_a*> aToStart;
         vector<node_a*> bToStart;
@@ -186,8 +277,20 @@ private:
                 return node;
             }
         }
+        return map[0][0];
     }
 
+    /*
+        The biggest change from the theoretical A* algorithm is that
+        in this assignment the Actor cannot simply 'teleport' around the map
+        to check the next node from 'moves'.
+
+        'aToB' function finds a path from node a to node b by considering
+        paths from a to (0;0) & from b to (0;0) using 'parent' pointers.
+        Afterwards, it finds the intersection of these paths and constructs an
+        way to get from node a to node b, while also calling 'dataAwait' after
+        every move.
+    */
     void aToB(node_a *a, node_a *b) {
         vector <node_a*> aToIntersection;
         vector <node_a*> intersectionToB;
@@ -205,23 +308,30 @@ private:
         }
         reverse(intersectionToB.begin(), intersectionToB.end());
 
-        aToIntersection.insert(aToIntersection.end(), intersectionToB.begin(), intersectionToB.end());
+        aToIntersection.insert(aToIntersection.end(), intersectionToB.begin() + 1, intersectionToB.end());
         for (auto el : aToIntersection) {
             printf("m %d %d\n", el->pos.x, el->pos.y);
             dataAwait();
         }
     }
 
+    /*
+        'iteration' function is responsible for choosing a next
+        node to check and end the program if the path is found or
+        if there are no more moves (-1).
+        The 'moves' list is sorted at the start of 'iteration'. The next 
+        node to move to is 'chosen' which is the 0th element of 'moves'.
+        If the next node is adjacent to the current position of the Actor,
+        then the algorithm simply moves to the next node.
+        Otherwise, it calls 'aToB'.
+        After, the 'chosen' move is removed from the 'moves' list.
+    */
     void iteration() {
         if (moves.empty()) {
             gameInProgress = false;
             return;
         }
         sort(moves.begin(), moves.end(), sorter);
-        // if (bdkPos.x + bdkPos.y >= 0 && find(moves.begin(), moves.end(), map[bdkPos.y][bdkPos.x]) != moves.end()) {
-        //     moves.erase(find(moves.begin(), moves.end(), map[bdkPos.y][bdkPos.x]));
-        //     moves.push_back(map[bdkPos.y][bdkPos.x]);
-        // }
         node_a *chosen = moves[0];
         if (neoPos == kmPos) {
             gameInProgress = false;
@@ -248,6 +358,13 @@ private:
     }
 
 public:
+
+    /*
+        Public constructor gets perception variant for this test
+        & coordinates of the Keymaker.
+        Then it calls 'fillMapValues' and adds the (0;0) node
+        to the 'moves' list as a starting point of the search.
+    */
     NeoAStar(int perceptionVariant, int kmX, int kmY) {
         this->perceptionVariant = perceptionVariant;
         this->kmPos = {kmX, kmY};
@@ -256,6 +373,10 @@ public:
         this->moves.push_back(map[0][0]);
     }
 
+
+    /*
+        The following getters & 'printMapValues' function were used while writing the code
+    */
     coord_t getKMPos() {
         return this->kmPos;
     }
@@ -278,6 +399,11 @@ public:
         }
     }
 
+    /*
+        Public function 'play' calls 'iteration' while the
+        game is in progress (indicated by 'gameInProgress' variable).
+        Afterwards, it prints the final answer.
+    */
     void play() {
         while (gameInProgress) {
             iteration();
@@ -286,12 +412,17 @@ public:
     }
 };
 
+/*
+    Input of perception variant and coordinates of the Keymaker
+    &
+    creation of instance 'neo' of 'NeoAStar' class
+    &
+    call of 'play' from 'neo'.
+*/
 int main() {
     int perceptVar, kmX, kmY;
     cin >> perceptVar >> kmX >> kmY;
 
     NeoAStar neo(perceptVar, kmX, kmY);
-    // neo.printMapValues();
     neo.play();
-    cout.flush();
 }
--- backtracking.cpp
+++ backtracking.cpp
@@ -1,7 +1,9 @@
+// Kirill Greshnov B23-DSAI-03
 #include <bits/stdc++.h>
 
 using namespace std;
 
+// coord_t struct holds the coordinates of a cell
 struct coord_t {
     int x;
     int y;
@@ -11,6 +13,23 @@ struct coord_t {
     }
 };
 
+/*
+    node_b struct represents a node/cell object
+
+    Variables:
+    pos - coordinates of a node
+    danger - 'true' if this node is dangerous to the Actor
+        (Enemy or Perseption Zone of the Enemy)
+    parent - pointer to the node the algorithm moved from
+        to this node
+    explored - 'true' if this node has been "explored" by the BFS
+        part of the algorithm
+    order - index of the node in a path from (0;0) to itself;
+        (0;0) gets order=0, which is incremented for next nodes
+        in the path
+    adjacent - vector of pointers to nodes, which are adjacent
+        to this node
+*/
 struct node_b {
     coord_t pos;
     bool danger = false;
@@ -25,15 +44,47 @@ struct node_b {
     }
 };
 
+/*
+    NeoBacktracking class constains the implementation of Backtracking algorithm
+        for this particular assignment
+
+    Explanation: consider that x and y are the coordinates of the
+        Keymaker, then, notice that, the shortest path to the Keymaker
+        cannot be shorter than (x + y). Therefore, initially, we can run a
+        modification of DFS algorithm, based of the rules of this assigment,
+        to find all paths of length (x + y). If the path of length (x + y) is
+        found, then it is the shortest and we can end the execution and output
+        the answer. If we haven't reaches Keymaker by following the above-
+        mentioned paths, then we can run a modified BFS algorithm from the ends
+        of those paths (nodes with order=(x + y)), to find the shortest path, if
+        is exists.
+
+    Variables:
+    perceptionVariant - perception variant of the actor for this test
+    map[][] - matrix of node objects (Environment of the game)
+    neoPos - current position of the Actor
+    kmPos - position of Keymaker (end point)
+    pathLength - length of the possible shortest path; this
+        value changes during the execution of the algorithm
+    bfsQ - queue of node pointers for the BFS part of this
+        algorithm
+
+    Functions:
+    (Comments are given above function)
+*/
 class NeoBacktracking {
 private:
     int perceptionVariant;
+    node_b* map[9][9] = {nullptr};
     coord_t neoPos = {0, 0};
     coord_t kmPos;
-    node_b* map[9][9] = {nullptr};
     int pathLength;
     queue <node_b*> bfsQ;
 
+    /*
+        The 'initAdj' adds the given node 'node' to its neighbours'
+        'adjacent' list.
+    */
     void initAdj(node_b *node) {
         if (node->pos.x > 0) {
             node->adjacent.push_back(map[node->pos.y][node->pos.x - 1]);
@@ -49,6 +100,10 @@ private:
         }
     }
 
+    /*
+        The 'initNodes' function creates node objects and adds them
+        to the 'map'. It also sets order of (0;0) node to 0.
+    */
     void initNodes() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -63,6 +118,11 @@ private:
         map[0][0]->order = 0;
     }
 
+    /*
+        The 'endangerNode' is a helper function which sets 'danger' value
+        of the node to 'true' and removes this node from the 'adjacent'
+        lists of its neighbours.
+    */
     void endangerNode(node_b *node) {
         if (node->danger) return;
         node->danger = true;
@@ -97,6 +157,10 @@ private:
         }
     }
 
+    /*
+        'handleAgent' function calls 'endangerNode' for the 'agentNode', as
+        well as for the nodes in this Agent's perception range
+    */
     void handleAgent(node_b *agentNode) {
         endangerNode(agentNode);
         for (int i = max(agentNode->pos.y - 1, 0); i < min(agentNode->pos.y + 1, 9); i++)
@@ -105,6 +169,9 @@ private:
             }
     }
 
+    /*
+        The 'handleSentinel' function works similarly to 'handleAgent
+    */
     void handleSentinel(node_b *sentinelNode) {
         endangerNode(sentinelNode);
         if (sentinelNode->pos.x > 0) endangerNode(map[sentinelNode->pos.y][sentinelNode->pos.x - 1]);
@@ -113,6 +180,12 @@ private:
         if (sentinelNode->pos.y < 8) endangerNode(map[sentinelNode->pos.y + 1][sentinelNode->pos.x]);
     }
 
+    /*
+        'dataAwait' function get information about nodes in Actor's perception
+        range after every move, it calls handlers for 'A'gents and 'S'entinel &
+        changes 'danger' values of nodes if they are said to be 'P'erception Zone
+        of the Enemy
+    */
     void dataAwait() {
         int k;
         cin >> k;
@@ -134,10 +207,22 @@ private:
         }
     }
 
+    /*
+        'isClose' function simply returns 'true' if the two given
+        nodes 'start' & 'end' are adjacent (or equal) to each other
+    */
     bool isClose(node_b *a, node_b *b) {
         return abs(a->pos.x - b->pos.x) + abs(a->pos.y - b->pos.y) <= 1;
     }
 
+    /*
+        The 'performMovementDfs' function handle movement of the Actor
+        during the DFS part of the algorithm. If the 'next' node is adjacent
+        to the current position of the Actor, then it will simply move to 'next'.
+        Otherwise, it will recreate a path from current node to 'next' using 'parent'
+        pointers to find these nodes' common node-ancestor and move through it,
+        while calling 'dataAwait' after every move.
+    */
     void performMovementDfs(node_b *next) {
         if (!isClose(next, map[neoPos.y][neoPos.x])) {
             node_b *currNode = map[neoPos.y][neoPos.x];
@@ -153,6 +238,9 @@ private:
         neoPos = next->pos;
     }
 
+    /*
+        The 'performMoveBfs' works very similarly to 'performMoveDfs'
+    */
     void performMoveBfs(node_b *next) {
         if (!isClose(next, map[neoPos.y][neoPos.x])) {
             node_b *currNode = map[neoPos.y][neoPos.x];
@@ -181,6 +269,11 @@ private:
         neoPos = next->pos;
     }
 
+    /*
+        DFS part of the algorithm implemented using a stack 's'.
+        The function adds nodes with order x + y (coordinates of
+        the Keymaker) to 'bfsQ'
+    */
     void dfs(node_b *node) {
         stack<node_b*> s;
         s.push(node);
@@ -194,32 +287,8 @@ private:
 
             if (next == map[kmPos.y][kmPos.x]) {
                 printf("e %d", pathLength);
-
-                // while (next != map[0][0]) {
-                //     printf("%d %d -- ", next->pos.x, next->pos.y);
-                //     next = next->parent;
-                // }
-
                 exit(0);
             }
-
-            // if (next->order > node->order + 1) {
-            //     next->parent = node;
-            //     next->order = node->order + 1;
-            // }
-
-            // if (!next->explored) {
-            //     next->explored = true;
-            //     if (next->order < pathLength) {
-            //         for (node_b *el : next->adjacent) {
-            //             if (!el->explored) {
-            //                 el->parent = next;
-            //                 el->order = next->order + 1;
-            //                 s.push(el);
-            //             }
-            //         }
-            //     }
-            // }
             if (next->order < pathLength) {
                 for (node_b *el : next->adjacent) {
                     if (el->order > next->order + 1 || el->order == -1) {
@@ -233,6 +302,14 @@ private:
         }
     }
 
+
+    /*
+        BFS part of the algorithm, slightly modified to
+        start not from one specific node, but already having
+        some paths done by DFS & a filled queue.
+        This function also returns "e -1" if the path doesn't
+        exist.
+    */
     void bfs() {
         while (!bfsQ.empty()) {
             node_b *next = bfsQ.front();
@@ -257,23 +334,37 @@ private:
         printf("e -1");
         exit(0);
     }
-    
-    void fullSearch() {
-        dfs(map[0][0]);
-        bfs();
-    }
 
 public:
+
+    /*
+        Public constructor gets perception variant for this test
+        & coordinates of the Keymaker.
+        Then it calls 'initNodes' to create node objects and
+        add them to the 'map'.
+    */
     NeoBacktracking(int perceptionVariant, int kmX, int kmY) : perceptionVariant(perceptionVariant), kmPos({kmX, kmY}) {
         pathLength = kmX + kmY;
         initNodes();
     }
 
+    /*
+        Public function 'play' calls DFS & BFS parts of the
+        algorithm in order.
+    */
     void play() {
-        fullSearch();
+        dfs(map[0][0]);
+        bfs();
     }
 };
 
+/*
+    Input of perception variant and coordinates of the Keymaker
+    &
+    creation of instance 'neo' of 'NeoAStar' class
+    &
+    call of 'play' from 'neo'.
+*/
 int main() {
     int perceptVar, kmX, kmY;
     cin >> perceptVar >> kmX >> kmY;
